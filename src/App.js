8721b2a72660fe985766b2149b015227645c183c
pyrolass <crowclaw935@gmail.com>
2022-05-22 14:58:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 function App() {
+  const [monsters, setMonster] = useState([]);
+  const [searchField, search] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonster(users));
+  }, []);
+
+  const filteredMonster = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input
+        type="search"
+        placeholder="search monsters"
+        onChange={(e) => search(e.target.value)}
+      /> */}
+      <h1>Monsters Roledex</h1>
+      <SearchBox
+        placeholder="search monsters"
+        handleChange={(e) => search(e.target.value)}
+      />
+      <CardList monsters={filteredMonster} />
     </div>
   );
 }
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import "./search-box.style.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <input
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+      className="search"
+    />
+  );
+};
